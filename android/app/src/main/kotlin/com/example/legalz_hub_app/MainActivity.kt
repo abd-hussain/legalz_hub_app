239142a7f099a800bd341e1ee4ce7b7b916614c8
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-03-30 17:06:42 +0300

    setup android

--- android/app/src/main/kotlin/com/example/legalz_hub_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/legalz_hub_app/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.legalz_hub_app
-
+package com.legalzhub.app
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
