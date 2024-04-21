a303328e236c4440ec400b8a18004d716a11f556
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-04-21 09:28:44 +0300

    add post and fix iOS

--- lib/screens/tabs/home_tab/widgets/add_post_bottomsheet.dart
+++ lib/screens/tabs/home_tab/widgets/add_post_bottomsheet.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:legalz_hub_app/models/https/categories_model.dart';
+import 'package:legalz_hub_app/screens/tabs/home_tab/widgets/add_post_category_bottomsheet.dart';
 import 'package:legalz_hub_app/shared_widget/bio_field.dart';
-import 'package:legalz_hub_app/shared_widget/booking/widgets/cell_of_booking.dart';
+import 'package:legalz_hub_app/shared_widget/bottom_sheet_util.dart';
 import 'package:legalz_hub_app/shared_widget/custom_button.dart';
 import 'package:legalz_hub_app/shared_widget/custom_text.dart';
 
@@ -11,15 +14,18 @@ import 'package:legalz_hub_app/shared_widget/custom_text.dart';
 //TODO category design and space
 //TODO: validate button before submit
 class AddPostBottomSheetsUtil {
-  ValueNotifier<Category?> selectedCategory = ValueNotifier<Category?>(null);
-
+  StreamController refreshPage = StreamController.broadcast();
   TextEditingController textController = TextEditingController();
+  File? image;
 
   Future bottomSheet(
       {required BuildContext context,
       required List<Category> categories,
-      required Function({required int catId, required String content, String? postImg}) addPost}) {
-    List<Category> listOfCategories = categories.where((s) => s.id != 0).toList();
+      required Function(
+              {required int catId, required String content, String? postImg})
+          addPost}) {
+    List<Category> listOfCategories =
+        categories.where((s) => s.id != 0).toList();
 
     return showModalBottomSheet(
         isScrollControlled: true,
@@ -30,162 +36,225 @@ class AddPostBottomSheetsUtil {
         ),
         context: context,
         builder: (context) {
-          return Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      child: IconButton(
-                        onPressed: () => Navigator.of(context).pop(),
-                        icon: const Icon(Icons.close),
-                      ),
-                    ),
-                    Expanded(
-                      child: CustomText(
-                        title: AppLocalizations.of(context)!.addnewquestion,
-                        textColor: const Color(0xff444444),
-                        textAlign: TextAlign.center,
-                        fontSize: 18,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100,
-                      child: CustomButton(
-                          enableButton: true,
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          }),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16, right: 16),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 3,
-                          offset: const Offset(0, 3), // changes position of shadow
+          return SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: IconButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          icon: const Icon(Icons.close),
                         ),
-                      ],
+                      ),
+                      Expanded(
+                        child: CustomText(
+                          title: AppLocalizations.of(context)!.addnewquestion,
+                          textColor: const Color(0xff444444),
+                          textAlign: TextAlign.center,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: CustomButton(
+                            enableButton: false,
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            }),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[100],
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 3,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomText(
+                              title: AppLocalizations.of(context)!
+                                  .tipsfornewquestiontitle,
+                              textColor: const Color(0xff444444),
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              maxLins: 2,
+                            ),
+                            CustomText(
+                              title:
+                                  "- ${AppLocalizations.of(context)!.tipsfornewquestion1}",
+                              textColor: const Color(0xff444444),
+                              fontSize: 11,
+                              maxLins: 2,
+                            ),
+                            CustomText(
+                              title:
+                                  "- ${AppLocalizations.of(context)!.tipsfornewquestion2}",
+                              textColor: const Color(0xff444444),
+                              fontSize: 11,
+                              maxLins: 2,
+                            ),
+                            CustomText(
+                              title:
+                                  "- ${AppLocalizations.of(context)!.tipsfornewquestion3}",
+                              textColor: const Color(0xff444444),
+                              maxLins: 2,
+                              fontSize: 11,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 16, right: 16, top: 16, bottom: 8),
+                    child: InkWell(
+                      child: Row(
                         children: [
-                          CustomText(
-                            title: AppLocalizations.of(context)!.tipsfornewquestiontitle,
-                            textColor: const Color(0xff444444),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                            maxLins: 2,
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Image.asset(
+                              "assets/images/avatar.jpeg",
+                              width: 30,
+                            ),
                           ),
-                          CustomText(
-                            title: "- ${AppLocalizations.of(context)!.tipsfornewquestion1}",
-                            textColor: const Color(0xff444444),
-                            fontSize: 11,
-                            maxLins: 2,
+                          const CustomText(
+                            title: "*",
+                            textColor: Colors.red,
+                            fontSize: 20,
                           ),
-                          CustomText(
-                            title: "- ${AppLocalizations.of(context)!.tipsfornewquestion2}",
-                            textColor: const Color(0xff444444),
-                            fontSize: 11,
-                            maxLins: 2,
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              border: Border.all(
+                                color: const Color(0xFFEEEEEE),
+                              ),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20)),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  CustomText(
+                                    title: AppLocalizations.of(context)!
+                                        .selectCategory,
+                                    textColor: const Color(0xff444444),
+                                    fontSize: 11,
+                                  ),
+                                  const Icon(Icons.arrow_drop_down_sharp),
+                                ],
+                              ),
+                            ),
                           ),
-                          CustomText(
-                            title: "- ${AppLocalizations.of(context)!.tipsfornewquestion3}",
-                            textColor: const Color(0xff444444),
-                            maxLins: 2,
-                            fontSize: 11,
+                        ],
+                      ),
+                      onTap: () {
+                        AddPostCategoryBottomSheetsUtil().bottomSheet(
+                          context: context,
+                          listOfCategories: listOfCategories,
+                          selectedCategoryCallBack: (cat) {},
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: BioField(
+                      bioController: textController,
+                      title: AppLocalizations.of(context)!.whatdoyouwanttoask,
+                      onChanged: (text) => validateFields(),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: InkWell(
+                      onTap: () {
+                        BottomSheetsUtil().addImageBottomSheet(
+                          context,
+                          image != null,
+                          AppLocalizations.of(context)!.profilephotosetting,
+                          AppLocalizations.of(context)!.setprofilephoto,
+                          deleteCallBack: () {
+                            // widget.attach1(null);
+                            image = null;
+                            // setState(() {});
+                            Navigator.pop(context);
+                          },
+                          cameraCallBack: () async {
+                            image = await pickImage(ImageSource.camera);
+                            if (image == null || image!.path.isEmpty) {
+                              return;
+                            }
+                            // widget.attach1(image);
+                            // setState(() {});
+                          },
+                          galleryCallBack: () async {
+                            image = await pickImage(ImageSource.gallery);
+                            if (image == null || image!.path.isEmpty) {
+                              return;
+                            }
+                            // widget.attach1(image);
+                            // setState(() {});
+                          },
+                        );
+                      },
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            "assets/images/attach_placeholder.png",
+                            width: 70,
+                            height: 70,
                           ),
+                          image != null
+                              ? Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.file(
+                                      image!,
+                                      width: 60,
+                                      height: 60,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                )
+                              : Container()
                         ],
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
-                  child: Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset(
-                          "assets/images/avatar.jpeg",
-                          width: 30,
-                        ),
-                      ),
-                      const Icon(Icons.arrow_drop_down_sharp),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[100],
-                          border: Border.all(
-                            color: Color(0xFFEEEEEE),
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: CustomText(
-                            title: "Select category",
-                            textColor: const Color(0xff444444),
-                            maxLins: 2,
-                            fontSize: 11,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                // ValueListenableBuilder<Category?>(
-                //     valueListenable: selectedCategory,
-                //     builder: (context, selectedCategoriesSnapshot, child) {
-                //       return SizedBox(
-                //         child: GridView.builder(
-                //             shrinkWrap: true,
-                //             physics: const NeverScrollableScrollPhysics(),
-                //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                //               crossAxisCount: 5,
-                //               mainAxisSpacing: 0,
-                //               crossAxisSpacing: 0,
-                //               childAspectRatio: 1,
-                //             ),
-                //             itemCount: listOfCategories.length,
-                //             itemBuilder: (context, index) {
-                //               return BookingCell(
-                //                 title: listOfCategories[index].name!,
-                //                 isSelected: selectedCategoriesSnapshot != null
-                //                     ? selectedCategoriesSnapshot == listOfCategories[index]
-                //                     : false,
-                //                 onPress: () {
-                //                   selectedCategory.value = listOfCategories[index];
-                //                 },
-                //               );
-                //             }),
-                //       );
-                //     }),
-                Padding(
-                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                  child: BioField(
-                    bioController: textController,
-                    title: AppLocalizations.of(context)!.whatdoyouwanttoask,
-                    onChanged: (text) => validateFields(),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         });
   }
 
   validateFields() {}
+
+  Future<File> pickImage(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+    return File(image?.path ?? "");
+  }
 }
---
+++ lib/screens/tabs/home_tab/widgets/add_post_category_bottomsheet.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:legalz_hub_app/models/https/categories_model.dart';
+import 'package:legalz_hub_app/shared_widget/custom_button.dart';
+import 'package:legalz_hub_app/shared_widget/custom_text.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:legalz_hub_app/shared_widget/booking/widgets/cell_of_booking.dart';
+
+class AddPostCategoryBottomSheetsUtil {
+  ValueNotifier<Category?> selectedCategory = ValueNotifier<Category?>(null);
+
+  Future bottomSheet(
+      {required BuildContext context,
+      required List<Category> listOfCategories,
+      required Function(Category) selectedCategoryCallBack}) {
+    return showModalBottomSheet(
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(25),
+          ),
+        ),
+        context: context,
+        builder: (context) {
+          return ValueListenableBuilder<Category?>(
+              valueListenable: selectedCategory,
+              builder: (context, selectedCategoriesSnapshot, child) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: IconButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            icon: const Icon(Icons.close),
+                          ),
+                        ),
+                        Expanded(
+                          child: CustomText(
+                            title: AppLocalizations.of(context)!.selectCategory,
+                            textColor: const Color(0xff444444),
+                            textAlign: TextAlign.center,
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 100,
+                          child: CustomButton(
+                              enableButton: selectedCategoriesSnapshot == null
+                                  ? false
+                                  : true,
+                              buttonTitle:
+                                  AppLocalizations.of(context)!.confirm,
+                              onTap: () {
+                                selectedCategoryCallBack(
+                                    selectedCategoriesSnapshot!);
+                                Navigator.of(context).pop();
+                              }),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            mainAxisSpacing: 0,
+                            crossAxisSpacing: 0,
+                            childAspectRatio: 1,
+                          ),
+                          itemCount: listOfCategories.length,
+                          itemBuilder: (context, index) {
+                            return BookingCell(
+                              title: listOfCategories[index].name!,
+                              isSelected: selectedCategoriesSnapshot != null
+                                  ? selectedCategoriesSnapshot ==
+                                      listOfCategories[index]
+                                  : false,
+                              onPress: () {
+                                selectedCategory.value =
+                                    listOfCategories[index];
+                              },
+                            );
+                          }),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                );
+              });
+        });
+  }
+}
